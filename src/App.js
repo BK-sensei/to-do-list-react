6e37b55cf3f373e942b2f866a87d81bbaeb34a55
becca <rebecca.kaci@gmail.com>
2021-11-02 11:37:22 +0100

    création d'un component form puis affichage du formulaire dans App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Form from './Components/Form';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+        <h1>To do list</h1>
+        <Form />
+      </>
     );
   }
 }
---
+++ src/Components/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text"/>
+                <button type="button">Submit</button>
+            </div>
+        );
+    }
+}
+
+export default Form;
